458ee634625f5c09cf38f3b3fac669b406b1464e
boxsocadmin <f1@clyst.it>
2023-04-10 13:42:58 +0000

    ngNOS Commit

--- python/ngnos/ifconfig/wireless.py
+++ python/ngnos/ifconfig/wireless.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-from vyos.ifconfig.interface import Interface
+from ngnos.ifconfig.interface import Interface
 
 @Interface.register
 class WiFiIf(Interface):
